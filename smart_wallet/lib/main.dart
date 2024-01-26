8257bcd7e86633754277886401b9287cf3297fe9
jadiel-bett <jadielbett@gmail.com>
2024-01-26 14:28:29 +0300

    Add dark theme, hide debug Banner

--- smart_wallet/lib/main.dart
+++ smart_wallet/lib/main.dart
@@ -10,8 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      home: const HomePageWidget(),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(brightness: Brightness.dark),
     );
   }
 }
