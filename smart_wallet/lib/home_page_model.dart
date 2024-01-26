a6c63cfffa3fe818484df89629f1a1d6b7822e31
jadiel-bett <jadielbett@gmail.com>
2024-01-26 14:16:24 +0300

    Add homePageModel

---
+++ smart_wallet/lib/home_page_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'home_page.dart' show HomePageWidget;
+import 'package:flutter/material.dart';
+
+class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  /// Initialization and disposal methods.
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
